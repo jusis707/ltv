328c9687adc0a06c44742160b11fae018365eff5
Juris Embergs <embergs.usa@gmail.com>
2024-04-07 13:56:26 +0300

    Update h2.sh

--- h2.sh
+++ h2.sh
@@ -88,6 +88,13 @@ clear
 minikube ip >ip-kube &
 kubectl  get services
 sleep 1
+pods_count=$(kubectl get pods -n $namespace | grep -c "Running")
+while [ $pods_count -ne $(kubectl get pods -n $namespace | grep -c "")]
+do
+ echo "waiting for all pods to be ready"
+ sleep 10
+done
+echo "All pods are ready"
 kubectl wait pod \
 --all \
 --for=condition=Ready \
