ee1a9efc2093a2ab50a718014cac7243dfb1ae5e
Juris Embergs <embergs.usa@gmail.com>
2024-04-07 12:11:26 +0300

    Update h2.sh

--- h2.sh
+++ h2.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 read -p "Are you sure? type y or no." -n 1 -r
-echo    # (optional) move to a new line
+echo  "UZMANIBU"  # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
 mkdir ~/ltv
